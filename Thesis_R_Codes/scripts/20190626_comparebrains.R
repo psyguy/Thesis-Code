d2ce15b38a8e9f13c21697538faaaf9bc08fc283
Manuel Haqi <mhscientist@gmail.com>
2019-06-27 15:09:02 +0200

    Plotting each round separately, looking at connectivities.

---
+++ Thesis_R_Codes/scripts/20190626_comparebrains.R
@@ -0,0 +1,158 @@
+rm(list = ls())
+
+# source("./functions/functions_trial.R")
+source("./functions/functions_netmeas.R")
+source("./functions/functions_partition.R")
+
+# for loop over all brain cases -------------------------------------------
+
+sampled.path <- "./data/"
+sampled.names <- list.files(path = sampled.path, pattern = "*.RData")
+sampled.names <- sampled.names[-length(sampled.names)]
+coefs_all <- NULL
+# rewires <- seq(1, 10001, 5)
+t <- Sys.time()
+for(sampled in sampled.names){
+  rm(brain_case)
+  load(paste(sampled.path, sampled, sep = ""))
+  brain_case@name %>% print()
+  coefs_all <- coefs_all %>%
+    rbind(brain_case@history$coefficients)
+}
+Sys.time()-t
+
+
+# plotting coefficients over time -----------------------------------------
+save_vars(list.of.vars = "coefs_all", prefix = "hpcJune26Harvest_tmp")
+coefficient.name <- (coefs_all %>% colnames())[-1:-6]
+
+# c <- coefs_all %>% filter(rewiring==1000) #filter(alphabeta.eps=="(0.5, 0.5)")
+
+coefs_all_mutated <- coefs_all %>% 
+  mutate(alphbet = paste0(alphabeta.eps,alphabeta.a))#,round)) %>% 
+  filter(round==2)
+
+for(i in coefficient.name){
+  this.coefficient <- coefs_all_mutated %>%
+    # filter(name == "Noa Adam") %>% 
+    # filter(alphabeta.eps=="(5, 1)", alphabeta.a=="(5, 1)") %>% 
+    select(i, rewiring, alphabeta.a, alphabeta.eps, name, alphbet)
+  colnames(this.coefficient)[1] <- "value"
+  # this.coefficient$value <- this.coefficient$value/co$value[1]
+  coef.plot <- this.coefficient %>% ggplot(aes(x = rewiring,
+                                               y = value,
+                                               colour = alphbet#,
+                                               # linetype = alphabeta.eps
+                                               )) +
+    geom_line(size = .75, alpha = 0.7) + 
+    ggplot2::ylim(0,NA) +
+    ggtitle(i) + theme(legend.position ="bottom")# c(0.7, 0.5))
+    # guides(fill=guide_legend(nrow=2,byrow=TRUE))
+  coef.plot %>% print()
+  # paste0(i, "_uniques.png") %>% ggsave()
+}
+
+
+
+diamonds_reshaped <- data.frame(price = diamonds$price,
+                                independent.variable = c(diamonds$carat,diamonds$cut,diamonds$color,diamonds$depth),
+                                Clarity = rep(diamonds$clarity,times=4),
+                                Variable.name = rep(c("Carat","Cut","Color","Depth"),each=nrow(diamonds)))
+
+ggplot(diamonds_reshaped,aes(independent.variable,price,colour=Clarity)) + 
+  geom_point(size=2) + facet_wrap(~Variable.name,scales="free_x") + 
+  xlab("")
+
+
+# Making coefs_all and plot pretty ----------------------------------------
+
+coefs <- coefs_all_mutated #%>% colnames()
+colnames(coefs) <- c("Owner", "Seed",
+                     "Round", "Epsilon Proportion",
+                     "a Proportion", "Rewiring",
+                     "Clustering", "Efficiency",
+                     "Small World", "Modularity",
+                     "Avg Path Length", "Proportions(eps)(a)")
+
+# coefs.long <- coefs %>% gather()
+
+library(ggpubr)
+
+
+
+for(i in 1:10){
+  coefs.this.round <- coefs %>% filter(Round==i)
+  owner.name <- coefs.this.round$Owner[1] %>% as.character()
+  title <- paste0("Coefficients for round ",
+                  i,
+                  " (",
+                  owner.name,
+                  ")")
+  p1 <- ggplot(data = coefs.this.round,
+               aes(x = Rewiring,
+                   y = Clustering,
+                   colour = `Proportions(eps)(a)`)) +
+    geom_line(size = .75, alpha = 0.7) + 
+    ggplot2::ylim(0,NA)
+  
+  p2 <- ggplot(data = coefs.this.round,
+               aes(x = Rewiring,
+                   y = `Small World`,
+                   colour = `Proportions(eps)(a)`)) +
+    geom_line(size = .75, alpha = 0.7) + 
+    ggplot2::ylim(0,NA)
+  
+  p3 <- ggplot(data = coefs.this.round,
+               aes(x = Rewiring,
+                   y = Modularity,
+                   colour = `Proportions(eps)(a)`)) +
+    geom_line(size = .75, alpha = 0.7) + 
+    ggplot2::ylim(0,NA)
+  
+  p4 <- ggplot(data = coefs.this.round,
+               aes(x = Rewiring,
+                   y = `Avg Path Length`,
+                   colour = `Proportions(eps)(a)`)) +
+    geom_line(size = .75, alpha = 0.7) + 
+    ggplot2::ylim(0,NA)
+  
+  figure <- ggarrange(p1, p2, p3, p4,
+                      ncol=2, nrow=2,
+                      common.legend = TRUE,
+                      legend="bottom"
+                      )
+  
+  annotate_figure(figure, top = title)
+  
+  paste0(title, ".png") %>% ggsave(width = 9,
+                                   dpi = "retina")
+}
+
+
+
+# looking at connectivities -----------------------------------------------
+
+load("I:/Thesis_Codes/Thesis_R_Codes/data/eps-0v6v0_a-0v5v1_r-3_g-0.3k-5.2k_David De Ridder_20190626_1905.RData")
+r3.olive <- brain_case
+load("I:/Thesis_Codes/Thesis_R_Codes/data/eps-1v5v0_a-0v6v0_r-3_g-0.3k-5.2k_Chiara Bosmans_20190626_1908.RData")
+r3.pink <- brain_case
+load("I:/Thesis_Codes/Thesis_R_Codes/data/eps-0v5v1_a-0v6v0_r-3_g-0.3k-5.2k_Daan Pauwels_20190626_1904.RData")
+r3.red <- brain_case
+rm(brain_case)
+
+m <- r3.olive@now$mat.connectivity
+m[1:50,1:50] <- m[1:50,1:50]*3
+m[1:50,51:300] <- m[1:50,51:300]*2
+m[51:300,1:50] <- m[51:300,1:50]*2
+
+# https://rstudio-pubs-static.s3.amazonaws.com/3486_79191ad32cf74955b4502b8530aad627.html
+pimage(m, col = c("white", "brown3",
+                  "darkorchid1", "blue2"))
+s <- seriate(m)
+pimage(m,s, col = c("white", "red", "black","blue"))
+g <- m %>% graph_from_adjacency_matrix(mode="undirected")
+g %>% plot(vertex.size=.1)
+
+
+
+
