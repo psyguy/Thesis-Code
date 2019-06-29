b2464c04ff2b7058b4531674994c546ced809654
Manuel Haqi <mhscientist@gmail.com>
2019-06-29 18:42:58 +0200

    Sending to HPC to extract coefficients

---
+++ Thesis_R_Codes/scripts/20190629_hpc_readcoefs.R
@@ -0,0 +1,23 @@
+#!/usr/bin/env Rscript
+
+Args <- commandArgs(TRUE)
+rounds <- as.numeric(Args[1])
+
+source("./functions/functions_reports.R")
+
+
+r_ <- (rounds %% 10) + 1
+
+ed_ <- rounds %/% 10
+
+pat.tmp <- NULL
+
+if(ed_ == 0) pat.tmp <- "_g-0.3k-5.2k"
+if(ed_ == 1) pat.tmp <- "_g-0.3k-4.68k"
+if(ed_ == 2) pat.tmp <- "_g-0.3k-5.72k"
+
+pattern <- paste0("r-", r_, pat.tmp)
+
+paste("Now reading round", r_, "of", pat.tmp, "index", rounds) %>% print()
+
+reports_coefs.extract(pattern = pattern)
