7fddb7b8d1b6c2594f5ab71813aeee2e339effde
Manuel Haqi <mhscientist@gmail.com>
2019-06-29 18:57:28 +0200

    HPC ready for coeffs

--- Thesis_R_Codes/scripts/20190629_hpc_readcoefs.R
+++ Thesis_R_Codes/scripts/20190629_hpc_readcoefs.R
@@ -8,13 +8,14 @@ source("./functions/functions_reports.R")
 
 r_ <- (rounds %% 10) + 1
 
-ed_ <- rounds %/% 10
+ed_ <- (rounds - 1) %/% 10
 
 pat.tmp <- NULL
 
 if(ed_ == 0) pat.tmp <- "_g-0.3k-5.2k"
 if(ed_ == 1) pat.tmp <- "_g-0.3k-4.68k"
 if(ed_ == 2) pat.tmp <- "_g-0.3k-5.72k"
+if(ed_ == 3) pat.tmp <- "_g-0.3k-6.24k"
 
 pattern <- paste0("r-", r_, pat.tmp)
 
