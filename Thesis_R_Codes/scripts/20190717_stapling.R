7997bdf487fad4d0c4078633cbc21b11df45f24d
psyguy <mhscientist@gmail.com>
2019-07-18 11:53:35 +0200

    stapling done, no more visualization to make, PDFs are on osf

---
+++ Thesis_R_Codes/scripts/20190717_stapling.R
@@ -0,0 +1,70 @@
+#!/usr/bin/env Rscript
+
+# Args <- commandArgs(TRUE)
+# rounds <- as.numeric(Args[1])
+rm(list = ls())
+
+source("./functions/functions_extract.R")
+
+pdf.names <- list.files(path = "./figures/connectivity-plots_25k",
+                        pattern = "*.pdf")
+
+pdf.names <- pdf.names[grepl("at 1000k rewirings_connectivities",
+                             pdf.names)]
+names <- gsub(" at 1000k rewirings_connectivities",
+              "",
+              pdf.names)
+
+# Sys.time()
+# for(i in seq_along(names)){
+#   print(i)
+#   pages <- (1+ (i-1)*40):(i*40)
+#   staplr::select_pages(pages,
+#                        "./figures/Network structure - all 25k.pdf",
+#                        paste("./figures/Network maturation of", names[i]))
+# }
+# Sys.time()
+
+
+
+# making pdfs of only adj mats --------------------------------------------
+
+pdf.names <- list.files(path = "./figures/connectivity-plots_25k",
+                        pattern = "*.pdf")
+
+pdf.names <- pdf.names[grepl("rewirings_connectivities",
+                             pdf.names)]
+
+names <- gsub(" at.*",
+              "",
+              pdf.names) %>% unique()
+
+Sys.time()
+for(i in seq_along(names)){
+  print(i)
+  pages <- (1+ (i-1)*40):(i*40)
+  staplr::select_pages(pages,
+                       "./figures/Network structure - all 25k.pdf",
+                       paste("./figures/Network maturation of", names[i]))
+}
+Sys.time()
+
+
+
+
+
+path.to.pdfs <- "./figures/netstats-plots_25k"
+coef.files <- list.files(path = path.to.pdfs, pattern = "*.pdf")
+
+p <- coef.files[grepl("homo", coef.files)] %>% 
+  c(coef.files[grepl("hypo-cha", coef.files)]) %>% 
+  c(coef.files[grepl("hyper-cha", coef.files)]) %>% 
+  c(coef.files[grepl("hypo-cou", coef.files)]) %>% 
+  c(coef.files[grepl("hyper-cou", coef.files)])
+
+
+
+staple_pdf(input_files = paste(path.to.pdfs, p, sep = "/"),
+           output_filepath = NULL)
+
+?savePlot
