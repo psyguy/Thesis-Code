0cd933d43781431716835c324fb089860019953a
psyguy <mhscientist@gmail.com>
2019-09-23 21:48:15 +0200

    initial results of clustering, for eigen vectors of connectivities. have to correct for svd

--- Thesis_R_Codes/scripts/20190923_try-svd.R
+++ Thesis_R_Codes/scripts/20190923_try-svd.R
@@ -12,41 +12,92 @@ e200 %*% e100
 
 matsim <- function(v1, v2){
   
-  eig1 <- v1 %>% vec2mat() %>% eigen(symmetric = TRUE)
-  eig2 <- v2 %>% vec2mat() %>% eigen(symmetric = TRUE)
+  m1 <- v1 %>% vec2mat()
+  m2 <- v2 %>% vec2mat()
+  
+  s1 <- m1 %>% seriate()
+  s2 <- m2 %>% seriate()
+  
+  eig1 <- m1[s1[[1]],s1[[2]]] %>% eigen(symmetric = TRUE)
+  eig2 <- m2[s2[[1]],s2[[2]]] %>% eigen(symmetric = TRUE)
   
   sim <- eig1$values %*% eig2$values
   sim %>% return()
 }
 
+snpmatsim <- function(r1, r2, snp){
+  matsim(snp$adj.mat.vect[[r1]],snp$adj.mat.vect[[r2]])
+}
+
+eigenmaker <- function(v){
+  
+  m <- v %>% vec2mat()
+
+  s <- m %>% seriate()
+
+  eig <- m[s[[1]], s[[2]]] %>% eigen(symmetric = TRUE)
+  
+  eig$values %>% return()
+}
+
+
+snp.whole <- snp %>% filter(Partition=="whole")
+
+e <- snp.whole$adj.mat.vect %>% map(eigenmaker)
+
+snpmatsim(1, 2, snp.whole)
+
+num.persons <- 50
+eigensim <- matrix(0, num.persons, nrow = num.persons)
+for(i in 1:num.persons){
+  for(j in 1:num.persons){
+    eigensim[i,j] <- e[[i]] %*% e[[j]]
+  }
+}
+
+e.centered <- eigensim-(max(eigensim)+min(eigensim))/2
+es <- e.centered %>% seriate()
+
+pimage(e.centered, key=F)
+pimage(e.centered, es, key=F)
+
+ee <- e %>% simplify2array %>% t()
+
+k <- kmeans(ee, 5)
+
+
+k$cluster %>% hist()
+
+cl <- k$cluster %>% cbind(snp.whole$Verbal.Description %>% as.character())
 
 
 
-#######################
-#Spectral Clustering
-######################
-#Gaussian mixtures generation
-library(MASS)#load MASS library
-Sigma = diag(2)/10#Diagonal covariance matrix
-data1 = mvrnorm(100,c(0,0),Sigma)#group 1 with mean (0,0)
-data2 = mvrnorm(100,c(2,0),Sigma)#group 2 with mean (2,0)
-data3 = mvrnorm(100,c(0,4),Sigma)#group 3 with mean (0,4)
-#Concatenate 3 gaussian clusters
-data = rbind(data1,data2,data3)
-#Linear covariance matrix computation
-scal_matrix = data%*%t(data)
-Kmatrix = as.matrix(dist(data,method='euclidean'))
-#Threshold and construction of the similarity-based adjacency matrix
-epsilon = 0.5
-A = (Kmatrix*epsilon)*1
-#Igraph construction based on the Adjacency matrix
-g = graph_from_adjacency_matrix(A,mode = 'undirected')
-#Graph Laplacian computation
-degrees = apply(A, 1,sum)
-L = diag(degrees)/300-A
-#Spectral clustering : 5 lines of code
-eigvalues = eigen(L)$values#SVD decomposition
-eigV =eigen(L)$vectors#Keep eigenvectors
-eigV = eigV [,300:1]#re-ordering of the eigenvectors
-U = eigV[,1:10]#Projection
-xx <- kmeans(U,4)#Clustering
+# 
+# #######################
+# #Spectral Clustering
+# ######################
+# #Gaussian mixtures generation
+# library(MASS)#load MASS library
+# Sigma = diag(2)/10#Diagonal covariance matrix
+# data1 = mvrnorm(100,c(0,0),Sigma)#group 1 with mean (0,0)
+# data2 = mvrnorm(100,c(2,0),Sigma)#group 2 with mean (2,0)
+# data3 = mvrnorm(100,c(0,4),Sigma)#group 3 with mean (0,4)
+# #Concatenate 3 gaussian clusters
+# data = rbind(data1,data2,data3)
+# #Linear covariance matrix computation
+# scal_matrix = data%*%t(data)
+# Kmatrix = as.matrix(dist(data,method='euclidean'))
+# #Threshold and construction of the similarity-based adjacency matrix
+# epsilon = 0.5
+# A = (Kmatrix*epsilon)*1
+# #Igraph construction based on the Adjacency matrix
+# g = graph_from_adjacency_matrix(A,mode = 'undirected')
+# #Graph Laplacian computation
+# degrees = apply(A, 1,sum)
+# L = diag(degrees)/300-A
+# #Spectral clustering : 5 lines of code
+# eigvalues = eigen(L)$values#SVD decomposition
+# eigV =eigen(L)$vectors#Keep eigenvectors
+# eigV = eigV [,300:1]#re-ordering of the eigenvectors
+# U = eigV[,1:10]#Projection
+# xx <- kmeans(U,4)#Clustering
