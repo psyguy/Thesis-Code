44053e3d2df2d933df3ab492b3ef6c1c70868985
psyguy <mhscientist@gmail.com>
2019-10-07 20:45:19 +0200

    going back to RV and MatrixCorrelation package

--- Thesis_R_Codes/scripts/20191007_try-decomposition.R
+++ Thesis_R_Codes/scripts/20191007_try-decomposition.R
@@ -6,6 +6,57 @@ load("./data-pc/snp_only-1e+6_20190723_1404.RData")
 m <- snp$adj.mat.vect[[1]] %>% vec2mat()
 
 
+# SVD and eigen stuff -----------------------------------------------------
+
+
+o.eigendecom <- miRNAss::eigenDecomposition(m,1)#$U
+# o.eigen <- eigen(m)
+s <- m %>% seriate()
+ser <- m[s[[1]], s[[2]]]
+o.svd <-  ser %>% svd()
+
+
+o.eigendecom <- miRNAss::eigenDecomposition(ser,1)#$U
+
+
+e.v <- matrix(0,300,300)
+e.v[1,] <- o.eigendecom$U
+mid <- matrix(0,300,300)
+mid[1,1] <- o.eigendecom$D
+
+
+mid <- diag(o.svd$d)
+red <- 300:300
+left <- o.svd$u
+left[red,] <- 0
+left <- left %>% t()
+
+(x <- left %*% mid %*% t(left)) %>%
+  pimage(key = FALSE, main = "kill me")
+
+
+
+(x <- e.v %*% mid %*% t(e.v)) %>%
+  pimage(key = FALSE, main = "kill me")
+
+d.red[red, red] <- 0
+
+
+
+(m.rec <- o.svd$v %*% d.red) %>% 
+  pimage(key = FALSE, main = "seriated")
+
+
+o.svd$u %>% pimage(key = FALSE)
+o.svd$v %>% pimage(key = FALSE)
+m.rec %>% pimage(key = FALSE, main = "reduced")
+ser %>% pimage(key = FALSE, main = "seriated")
+
+(m.rec <- o.svd$v - (o.svd$u)) %>% 
+  pimage()#key = FALSE, main = "seriated")
+
+sum(m-m.rec)
+
 
 svdmaker <- function(v){
   m <- v %>% vec2mat()
@@ -13,6 +64,9 @@ svdmaker <- function(v){
   eig <- m[s[[1]], s[[2]]] %>% svd()
   eig$d %>% return()
 }
+
+eig$v %>% pimage
+
 
 svd.e <- eig$d
 
@@ -58,32 +112,12 @@ h <- ee %>%
 
 h %>% plot()
 
-# 
-# #######################
-# #Spectral Clustering
-# ######################
-# #Gaussian mixtures generation
-# library(MASS)#load MASS library
-# Sigma = diag(2)/10#Diagonal covariance matrix
-# data1 = mvrnorm(100,c(0,0),Sigma)#group 1 with mean (0,0)
-# data2 = mvrnorm(100,c(2,0),Sigma)#group 2 with mean (2,0)
-# data3 = mvrnorm(100,c(0,4),Sigma)#group 3 with mean (0,4)
-# #Concatenate 3 gaussian clusters
-# data = rbind(data1,data2,data3)
-# #Linear covariance matrix computation
-# scal_matrix = data%*%t(data)
-# Kmatrix = as.matrix(dist(data,method='euclidean'))
-# #Threshold and construction of the similarity-based adjacency matrix
-# epsilon = 0.5
-# A = (Kmatrix*epsilon)*1
-# #Igraph construction based on the Adjacency matrix
-# g = graph_from_adjacency_matrix(A,mode = 'undirected')
-# #Graph Laplacian computation
-# degrees = apply(A, 1,sum)
-# L = diag(degrees)/300-A
-# #Spectral clustering : 5 lines of code
-# eigvalues = eigen(L)$values#SVD decomposition
-# eigV =eigen(L)$vectors#Keep eigenvectors
-# eigV = eigV [,300:1]#re-ordering of the eigenvectors
-# U = eigV[,1:10]#Projection
-# xx <- kmeans(U,4)#Clustering
+
+
+
+
+
+# RV and MatrixCorrelation ------------------------------------------------
+
+
+library("MatrixCorrelation")
