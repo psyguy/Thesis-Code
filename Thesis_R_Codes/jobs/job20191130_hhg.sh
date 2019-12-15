563c6fd550fbcdf31298b1960cb8b1dac26f4518
Manuel Haqi <mhscientist@gmail.com>
2019-12-15 12:11:50 +0100

    Starting HHG script without standardization

---
+++ Thesis_R_Codes/jobs/job20191130_hhg.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+#PBS -l nodes=5:ppn=30
+#PBS -l walltime=00:59:59
+#PBS -l pmem=6gb
+#PBS -N November30_hhg
+#PBS -m b -M mhscientist@gmail.com
+ 
+export OPENBLAS_NUM_THREADS=1
+export GOTO_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+ 
+ 
+cd $PBS_O_WORKDIR
+ 
+ 
+module load R/3.6.0-foss-2018a-bare
+ 
+./scripts/20191130_hpc_hhg.R $counter
